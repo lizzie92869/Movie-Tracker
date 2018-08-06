464492ead16ab761eb0cf880d485c3dec410ee5b
Lizzie <lizzie.sorin@hotmail.fr>
2018-08-06 14:22:13 -0700

    icons on Watched and To Watch navigation

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -9,8 +9,8 @@ const NavBar = props => {
   return (
     <div className="nav-bar">
      <NavLink to="/" exact className="nav-button" activeClassName="active-nav-button" >Films</NavLink> 
-     <NavLink to="/watched" exact className="nav-button" activeClassName="active-nav-button" id="button-watched">Watched</NavLink>
-     <NavLink to="/towatch" exact className="nav-button" activeClassName="active-nav-button" id="button-to-watch">To Watch</NavLink>
+     <NavLink to="/watched" exact className="nav-button" activeClassName="active-nav-button" id="button-watched"><i class="small material-icons">playlist_add_check</i>Watched</NavLink>
+     <NavLink to="/towatch" exact className="nav-button" activeClassName="active-nav-button" id="button-to-watch"><i class="small material-icons">playlist_add</i>To Watch</NavLink>
     </div>
   );
 };
