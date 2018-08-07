60381783627a337b273d6ab420e368fecbf4814d
Lizzie <lizzie.sorin@hotmail.fr>
2018-08-07 14:10:58 -0700

    switch for container component with a handleWatchedClick()

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,20 +1,64 @@
-import React from 'react';
+// import React from 'react';
+// import FilmsPage from './FilmsPage'
+
+// const Movie = (props) => {
+
+// // handleWatchedClick = e => {
+// // console.log("ici")
+// // }
+
+// // handleToWatchClick = (e) => {
+
+// // }
+
+// // handleNotInterestedClick = (e) => {
+
+// // }	
+
+// const film = props.film
+
+// 	return ( 
+// 	 <div className="block-movie">
+//      <a key={film.id} href="#"><img width="150" src={"https://image.tmdb.org/t/p/w185/"+film.poster_path}/></a>
+//     <div>
+//     	<button onClick={this.handleWatchedClick} className="waves-effect waves-light btn btn-small green"><i class="small material-icons">playlist_add_check</i></button>  
+//     	<a className="waves-effect waves-light btn btn-small green"><i class="small material-icons">playlist_add</i></a>
+//     	<a className="waves-effect waves-light btn btn-small grey"><i class="small material-icons">remove_circle_outline</i></a>
+//     </div>
+//     </div>
+// 	)
+// }
+
+// export default Movie
+
+
+
+
+
+
+import React, { Component } from 'react';
 import FilmsPage from './FilmsPage'
 
-const Movie = (props) => {
+class Movie extends Component {
 
-const film = props.film
+handleWatchedClick = e => {
+console.log("ici")
+}
 
-	return ( 
+
+
+	render() {
+    return (
 	 <div className="block-movie">
-     <a key={film.id} href="#"><img width="150" src={"https://image.tmdb.org/t/p/w185/"+film.poster_path}/></a>
+     <a key={this.props.film.id} href="#"><img width="150" src={"https://image.tmdb.org/t/p/w185/"+this.props.film.poster_path}/></a>
     <div>
-    	<a className="waves-effect waves-light btn btn-small green"><i class="small material-icons">playlist_add_check</i></a>  
+    	<button onClick={this.handleWatchedClick} className="waves-effect waves-light btn btn-small green"><i class="small material-icons">playlist_add_check</i></button>  
     	<a className="waves-effect waves-light btn btn-small green"><i class="small material-icons">playlist_add</i></a>
     	<a className="waves-effect waves-light btn btn-small grey"><i class="small material-icons">remove_circle_outline</i></a>
     </div>
     </div>
 	)
+	}
 }
 
 export default Movie
