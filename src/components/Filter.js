e97de4d43e72848fd7be43605fbfe2cd650b1f97
Lizzie <lizzie.sorin@hotmail.fr>
2018-08-07 08:58:02 -0700

    connecting filter to the store and change state onChange

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,38 +1,117 @@
-import React from 'react';
+import React, { Component } from 'react';
+import {connect} from 'react-redux';
+import { bindActionCreators } from 'redux';
+import * as actions from '../actions/movieActions.js'
 
-const Filter = () => 
-  <div>
-  <p> Here will be a movie filter to find more targetted suggestions or a title in particular </p>
-  {/*<form onChange="filterMovies()">*/}
-    <div className="row">
-    <form className="col s12">
-      <div className="row">
-        <div className="input-field col s5">
-          <input placeholder="Title" id="first_name" type="text" className="validate" />
-        </div>
-        <div className="input-field col s2">
-          <input placeholder="Year" id="last_name" type="text" className="validate" />
-        </div>
-        <div className="input-field col s3">
-          <input placeholder="Genre" id="last_name" type="text" className="validate" />
-        </div>
-        <div className="input-field col s2">
-          <div><label>
-            <input name="group1" type="radio" checked className="input-field col s2" />
-            <span>Sort by Date</span>
-          </label>
+class Filter extends Component {
+  // constructor(props) {
+  //   super(props)
+
+  // }
+
+
+
+  state = {
+    searchTerm: '',
+    searchGenre: '', 
+    searchYear: '',
+    searchPopularity: true,
+    searchDate: false
+  }
+
+  filterMovies = (e) => {
+
+  }
+
+  updateSearchTerm = e => {
+    this.setState({
+      searchTerm: e.target.value,
+    })
+    console.log("state:", this.state)
+    console.log("this.props:",this.props)
+    console.log("this.props.moviesFiltered:", this.props.moviesFiltered)
+    console.log("this.props.actions", this.props.actions)
+    this.props.actions.fetchFilteredMovies()
+  }
+
+  updateSearchGenre = e => {
+    this.setState({
+      searchGenre: e.target.value,
+    })
+    console.log(this.state)
+  }
+
+  updateSearchYear = e => {
+    this.setState({
+      searchYear: e.target.value,
+    })
+    console.log(this.state)
+  }
+
+  // updateSearchPopularity = e => {
+  //   this.setState({
+  //     searchPopularity: e.target.value,
+  //   })
+  // }
+
+  // updateSearchDate = e => {
+  //   this.setState({
+  //     searchDate: e.target.value,
+  //   })
+  // }
+
+
+  render() {
+    return (
+ 
+      <div>
+      <p> Here will be a movie filter to find more targetted suggestions or a title in particular </p>
+
+        <div className="row">
+        <form className="col s12" onSubmit={this.filterMovies}>
+          <div className="row">
+            <div className="input-field col s5">
+              <input onChange={this.updateSearchTerm} placeholder="Title" id="first_name" type="text" className="validate" />
+            </div>
+            <div className="input-field col s2">
+              <input  onChange={this.updateSearchYear} placeholder="Year" id="year" type="text" className="validate" />
+            </div>
+            <div className="input-field col s3">
+              <input onChange={this.updateSearchGenre} placeholder="Genre" id="year" type="text" className="validate" />
+            </div>
+            <div className="input-field col s2">
+              <div><label>
+                <input name="group1" type="radio" checked className="input-field col s2" />
+                <span>Sort by Date</span>
+              </label>
+              </div>
+              <div>
+              <label>
+                <input name="group1" type="radio" checked className="input-field col s2s"/>
+                <span>or Popularity</span>
+              </label>
+              </div>
+            </div>
           </div>
-          <div>
-          <label>
-            <input name="group1" type="radio" checked className="input-field col s2s"/>
-            <span>or Popularity</span>
-          </label>
-          </div>
+        </form>
         </div>
       </div>
-    </form>
-  </div>
+      )
+  }
+}
 
-  </div>
+function mapStateToProps(state) {
 
-  export default Filter
+  console.log('in MapStateToProps in Filter.js')
+  return {
+    moviesFiltered: state.movies.moviesFiltered,
+    moviesWatched: state.movies.moviesWatched,
+    moviesToWatch: state.movies.moviesToWatch
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return {actions: bindActionCreators(actions, dispatch)}
+}
+
+export const ConnectedFilter = connect(mapStateToProps, mapDispatchToProps)(Filter)
