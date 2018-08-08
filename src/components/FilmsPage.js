bdbd66960ed65a974df60b5018bd9681c280512e
Lizzie <lizzie.sorin@hotmail.fr>
2018-08-08 11:39:41 -0700

    ToWatchPage displaying the movies in the moviesToWatch state

--- src/components/FilmsPage.js
+++ src/components/FilmsPage.js
@@ -5,16 +5,12 @@ import {ConnectedMovie} from './Movie';
 import {ConnectedFilter} from './Filter';
 
 
-
-
-
 const FilmsPage = (props) => {
   const movies = props.moviesFiltered.map(
     film => <ConnectedMovie film={film}/>)
 
   return (
   <div className="frameDisplayMovies">
-  {/*<Filter moviesFiltered={props.moviesFiltered}/>*/}
   <ConnectedFilter />
   <div><p>Here is a list of suggestion from popular movies.</p> 
   <p>If the movie is in one of the list, it won't be shown here</p></div>
--- src/components/ToWatchPage.js
+++ src/components/ToWatchPage.js
@@ -1,14 +1,21 @@
 
 import React from 'react';
+import {ConnectedMovie} from './Movie';
 // import ToWatchMovies from './ToWatchMovies'
 
 const ToWatchPage = (props) => {
+console.log("props from ToWatchPage", props)
+
+	const movies = props.moviesToWatch.map(
+			film => <ConnectedMovie film={film} />
+		)
+
 return (
   <div className="frameDisplayMovies">
   <p>Here is the list of movies I want to see.</p> 
-{/*  <ToWatchMovies/>*/}
+	{movies}
   </div>
 )
 }
 
-  export default ToWatchPage
+export default ToWatchPage
