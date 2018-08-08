4e2adcfd39501283c55cfd3d46a30ba8f3b2eb7a
Lizzie <lizzie.sorin@hotmail.fr>
2018-08-08 09:26:23 -0700

    fetch movies by title is searching with a character off

--- src/actions/movieActions.js
+++ src/actions/movieActions.js
@@ -11,39 +11,27 @@ export function fetchMovies() {
 
     dispatch({ type: 'LOADING_MOVIES' });
     return fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${moviesDBKey}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1`)
-              .then(response => response.json())
-              .then(responseData => {
-              	//dispatch an action with a type and a payload
-              	const movies = responseData.results
-              	dispatch({ type: 'FETCH_MOVIES', payload: movies}) 
-                });            
+      .then(response => response.json())
+      .then(responseData => {
+      	//dispatch an action with a type and a payload
+      	const movies = responseData.results
+      	dispatch({ type: 'FETCH_MOVIES', payload: movies}) 
+        });            
   };
-
 }
 
-// export function fetchFilteredMovies() {
-// console.log("in fetchFilteredMovies()")
-//   return (dispatch) => {
 
-//     const moviesDBKey = process.env.REACT_APP_MOVIEDB_KEY
-//     console.log("log:", moviesDBKey)
-//     //const sorting = //"date" or "popularity"
-//     //const searchTerm =
-//     //const year = 
+export const fetchMoviesByTitle = (searchTerm) => {
+  return (dispatch) => { 
+    const moviesDBKey = process.env.REACT_APP_MOVIEDB_KEY
 
-
-//     // debugger
-
-//     dispatch({ type: 'LOADING_MOVIES' });
-//     return fetch(`https://api.themoviedb.org/3/search/movie?api_key=${moviesDBKey}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1`)
-//            //https://api.themoviedb.org/3/search/movie?api_key=${moviesDBKey}&language=en-US&query={searchTerm}&year={year}&sort_by={sorting}.desc&include_adult=false
-//               .then(response => response.json())
-//               .then(responseData => {
-//                 //dispatch an action with a type and a payload
-//                 const movies = responseData.results
-//                 dispatch({ type: 'FETCH_MOVIES', payload: movies})
-//                 console.log(this)})
-             
-//   };
-
-// }
+    dispatch({ type: 'LOADING_MOVIES' });
+    return fetch(`https://api.themoviedb.org/3/search/movie?api_key=${moviesDBKey}&query=${searchTerm}`)
+      .then(response => response.json())
+      .then(responseData => {
+        //dispatch an action with a type and a payload
+        const movies = responseData.results
+        dispatch({ type: 'FETCH_MOVIES', payload: movies})
+      });
+  };         
+}
