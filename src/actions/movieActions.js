624cc00f6f9c3d18daec3d4a9892ed8596d74573
Lizzie <lizzie.sorin@hotmail.fr>
2018-08-13 11:44:08 -0700

    the fetchs are now retrieving 60 movies instead of 20

--- src/actions/movieActions.js
+++ src/actions/movieActions.js
@@ -6,17 +6,30 @@ export function fetchMovies() {
   return (dispatch) => {
 
   	const moviesDBKey = process.env.REACT_APP_MOVIEDB_KEY
-  	console.log("log:", moviesDBKey)
-  	// debugger
-
     dispatch({ type: 'LOADING_MOVIES' });
+
     return fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${moviesDBKey}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1`)
       .then(response => response.json())
       .then(responseData => {
       	//dispatch an action with a type and a payload
-      	const movies = responseData.results
-      	dispatch({ type: 'FETCH_MOVIES', payload: movies}) 
-        });            
+      	const movies1 = responseData.results
+
+        fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${moviesDBKey}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=2`)
+        .then(response => response.json())
+        .then(responseData => {
+          const movies2 = responseData.results
+          const movies12 = movies1.concat(movies2)
+
+          fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${moviesDBKey}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=3`)
+        .then(response => response.json())
+        .then(responseData => {
+          const movies3 = responseData.results
+          const movies123 = movies12.concat(movies3)
+
+          dispatch({ type: 'FETCH_MOVIES', payload: movies123}) 
+          });
+        });   
+      });          
   };
 }
 
@@ -26,12 +39,27 @@ export const fetchMoviesByTitle = (searchTerm) => {
     const moviesDBKey = process.env.REACT_APP_MOVIEDB_KEY
 
     dispatch({ type: 'LOADING_MOVIES' });
-    return fetch(`https://api.themoviedb.org/3/search/movie?api_key=${moviesDBKey}&query=${searchTerm}`)
+    return fetch(`https://api.themoviedb.org/3/search/movie?api_key=${moviesDBKey}&language=en-US&query=${searchTerm}&page=1`)
       .then(response => response.json())
       .then(responseData => {
-        //dispatch an action with a type and a payload
-        const movies = responseData.results
-        dispatch({ type: 'FETCH_MOVIES', payload: movies})
+        const movies1 = responseData.results
+
+        fetch(`https://api.themoviedb.org/3/search/movie?api_key=${moviesDBKey}&language=en-US&query=${searchTerm}&page=2`)
+      .then(response => response.json())
+      .then(responseData => {
+        const movies2 = responseData.results
+        const movies12 = movies1.concat(movies2)
+
+          fetch(`https://api.themoviedb.org/3/search/movie?api_key=${moviesDBKey}&language=en-US&query=${searchTerm}&page=2`)
+          .then(response => response.json())
+          .then(responseData => {
+            const movies3 = responseData.results
+            const movies123 = movies12.concat(movies3)
+
+            dispatch({ type: 'FETCH_MOVIES', payload: movies123})
+
+          });
+        });
       });
   };         
 }
@@ -45,47 +73,29 @@ export const fetchMoviesByPreferences = (obj) => {
     const moviesDBKey = process.env.REACT_APP_MOVIEDB_KEY
 
     dispatch({ type: 'LOADING_MOVIES' });
-    return fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${moviesDBKey}&primary_release_year=${obj.searchYear||""}&with_genres=${obj.genreId||""}&sort_by=${obj.sorting}&language=en-US&include_video=false&include_adult=false`)
+    return fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${moviesDBKey}&primary_release_year=${obj.searchYear||""}&with_genres=${obj.genreId||""}&sort_by=${obj.sorting}&language=en-US&include_video=false&include_adult=false&page=1`)
       .then(response => response.json())
       .then(responseData => {
-        //dispatch an action with a type and a payload
-        const movies = responseData.results
-        dispatch({ type: 'FETCH_MOVIES', payload: movies})
+        const movies1 = responseData.results
+
+      fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${moviesDBKey}&primary_release_year=${obj.searchYear||""}&with_genres=${obj.genreId||""}&sort_by=${obj.sorting}&language=en-US&include_video=false&include_adult=false&page=2`)
+      .then(response => response.json())
+      .then(responseData => {
+        const movies2 = responseData.results
+        const movies12 = movies1.concat(movies2)
+
+          fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${moviesDBKey}&primary_release_year=${obj.searchYear||""}&with_genres=${obj.genreId||""}&sort_by=${obj.sorting}&language=en-US&include_video=false&include_adult=false&page=3`)
+          .then(response => response.json())
+          .then(responseData => {
+            const movies3 = responseData.results
+            const movies123 = movies12.concat(movies3)
+
+          dispatch({ type: 'FETCH_MOVIES', payload: movies123})
+          });
       });
+    });
   };         
 }
-
-// export const fetchMoviesByPreferences = (searchYear="", searchGenreId="", sorting="popularity") => {
-//   debugger
-//   return (dispatch) => { 
-//     const moviesDBKey = process.env.REACT_APP_MOVIEDB_KEY
-
-//     dispatch({ type: 'LOADING_MOVIES' });
-//     return fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${moviesDBKey}&primary_release_year=${searchYear}&with_genres=${searchGenreId}&language=en-US&sort_by=${sorting}&include_video=false&include_adult=false`)
-//       .then(response => response.json())
-//       .then(responseData => {
-//         //dispatch an action with a type and a payload
-//         const movies = responseData.results
-//         dispatch({ type: 'FETCH_MOVIES', payload: movies})
-//       });
-//   };         
-// }
-
-// export const fetchMoviesByGenre = (searchGenreId) => {
-//   debugger
-//   return (dispatch) => { 
-//     const moviesDBKey = process.env.REACT_APP_MOVIEDB_KEY
-
-//     dispatch({ type: 'LOADING_MOVIES' });
-//     return fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${moviesDBKey}&with_genres=${searchGenreId}&language=en-US&include_video=false&include_adult=false`)
-//       .then(response => response.json())
-//       .then(responseData => {
-//         //dispatch an action with a type and a payload
-//         const movies = responseData.results
-//         dispatch({ type: 'FETCH_MOVIES', payload: movies})
-//       });
-//   };         
-// }
 
 
 export const addFilmToWatchedList = film => ({
